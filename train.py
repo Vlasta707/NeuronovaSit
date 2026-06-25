4047f1ffd7b1d8b32738390580d8ea088a42eac5
Vlasta Štoček <vlasta.stocek@post.cz>
2026-06-25 15:28:58 +0200

    Vyladění definice architektury CNN podle návrhu AI ve skriptu train.py

--- train.py
+++ train.py
@@ -56,16 +56,22 @@ class PrumyslovaSit(nn.Module):
     def __init__(self):
         super(PrumyslovaSit, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)  # <-- NOVÉ: Normalizace
+        
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)  # <-- NOVÉ: Normalizace
+        
         self.fc1 = nn.Linear(64 * 32 * 32, 128)
         self.fc2 = nn.Linear(128, 2)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.5)
+        
+        self.relu = nn.LeakyReLU(0.1)  # <-- ZMĚNA: Pokročilejší aktivace místo nn.ReLU()
+        self.dropout = nn.Dropout(0.2)  # <-- ZMĚNA: Jemnější dropout pro menší dataset
 
     def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.bn1(self.conv1(x))))  # Přidáno bn1
+        x = self.pool(self.relu(self.bn2(self.conv2(x))))  # Přidáno bn2
         x = self.pool(x)
         x = x.view(x.size(0), -1)
         x = self.relu(self.fc1(x))
